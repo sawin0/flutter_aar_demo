9e95901c38978e5ed7f16454582a469da8c00c74
Sabin RanaBhat <binranabhat@gmail.com>
2023-07-17 11:40:31 +0000

    testing

--- MyApplication/app/src/main/java/com/sabin/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/sabin/myapplication/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         warmupFlutterEngine()
+        print('Hello')
 
         btn = findViewById(R.id.btn)
         btn.setOnClickListener {    runFlutterNPS()}
